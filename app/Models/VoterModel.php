7a65ea7c6a0a8af7e4a8e376bbdaa05a22597951
Art Zolina III <artchel06studios@yahoo.com>
2023-08-24 14:26:15 +0800

    edit error

--- app/Models/VoterModel.php
+++ app/Models/VoterModel.php
@@ -1,10 +1,10 @@
 <?php
 
 namespace App\Models;
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
 use CodeIgniter\Model;
 use CodeIgniter\Controller;
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 class VoterModel extends Model
 {
