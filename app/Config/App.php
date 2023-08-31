c4186edb0c4b93f9a3eb5002c5ea604df7d1e526
Art Zolina III <artchel06studios@yahoo.com>
2023-08-31 08:02:18 +0800

    edit getaddressbyid

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,8 +23,8 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'https://unligames.com/fairview';
-    //public $baseURL = 'http://localhost:8080';
+    //public $baseURL = 'https://unligames.com/fairview';
+    public $baseURL = 'http://localhost:8080';
 
 
     /**
